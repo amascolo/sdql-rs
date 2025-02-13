129f3432b152e237dd240af3013d6eae1ea27c6f
amascolo <12738821+amascolo@users.noreply.github.com>
2025-02-13 15:34:29 +0000

    cleanup

--- benches/tpch.rs
+++ benches/tpch.rs
@@ -1,38 +1,20 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use itertools::iproduct;
 use sdql::q3::parallel::q3_query_rayon;
 use sdql::q3::read::read_q3;
 use sdql::q3::sequential::q3_query;
 
-const CRITERION_MIN_SAMPLE_SIZE: usize = 10;
-const SCALE_FACTORS: &[&str] = &["0.01", "1"];
-const PARALLELISM: [bool; 2] = [false, true];
-
-fn benchmark_read_q3(_c: &mut Criterion) {
-    let mut c = Criterion::default().sample_size(CRITERION_MIN_SAMPLE_SIZE);
-    for sf in SCALE_FACTORS {
-        let path = |table| format!("datasets/tpch_datasets/SF_{sf}/{table}.tbl");
-        let id = BenchmarkId::new("q3", format!("SF{sf}"));
-        c.bench_with_input(id, &(), |b, _| {
-            b.iter(|| {
-                black_box(read_q3(&path("customer"), &path("orders"), &path("lineitem")).unwrap())
-            })
-        });
-    }
-}
-
-fn benchmark_query_q3(c: &mut Criterion) {
-    for (sf, parallel) in iproduct!(SCALE_FACTORS, PARALLELISM) {
-        let path = |table| format!("datasets/tpch_datasets/SF_{sf}/{table}.tbl");
-        let data = read_q3(&path("customer"), &path("orders"), &path("lineitem")).unwrap();
+fn benchmark_q3(c: &mut Criterion) {
+    let path = |table| format!("datasets/tpch_datasets/SF_1/{table}.tbl");
+    let data = read_q3(&path("customer"), &path("orders"), &path("lineitem")).unwrap();
+    for parallel in [false, true] {
         let query = if parallel { q3_query_rayon } else { q3_query };
         let variant = if parallel { "parallel" } else { "sequential" };
-        let id = BenchmarkId::new("q3", format!("SF{sf}_{variant}"));
+        let id = BenchmarkId::new("q3", format!("SF1_{variant}"));
         c.bench_with_input(id, &data, |b, (customer, orders, lineitem)| {
             b.iter(|| black_box(query(customer, orders, lineitem)))
         });
     }
 }
 
-criterion_group!(benches, benchmark_read_q3, benchmark_query_q3);
+criterion_group!(benches, benchmark_q3);
 criterion_main!(benches);
