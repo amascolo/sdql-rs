41a78e10a42feca7a28d1513c45a2b9c2ae23aef
amascolo <12738821+amascolo@users.noreply.github.com>
2025-02-24 15:18:45 +0000

    more elegant

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use prettyplease::unparse;
-use proc_macro2::{Span, TokenStream};
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{File, Ident};
+use syn::{parse_quote, parse_str, File, Type};
 
-fn generate_ast(type_name: &str, field_types: &[&str]) -> TokenStream {
-    let type_ident = Ident::new(type_name, Span::call_site());
+fn parse_type(t: &str) -> Type {
+    match t {
+        "i32" => parse_quote!(i32),
+        "String" => parse_quote!(String),
+        "f64" => parse_quote!(f64),
+        _ => parse_str(t).unwrap(),
+    }
+}
 
+fn generate_ast(type_name: &str, field_types: &[Type]) -> TokenStream {
+    let type_ident = syn::Ident::new(type_name, proc_macro2::Span::call_site());
     let field_definitions = field_types.iter().map(|ty| {
-        let ty_ident = Ident::new(ty, Span::call_site());
         quote! {
-            Vec<#ty_ident>,
+            Vec<#ty>,
         }
     });
-
     quote! {
         pub type #type_ident = (
             #(#field_definitions)*
@@ -22,9 +28,10 @@ fn generate_ast(type_name: &str, field_types: &[&str]) -> TokenStream {
 }
 
 fn main() {
-    let field_types = vec![
+    let dynamic_types = [
         "i32", "String", "String", "i32", "String", "f64", "String", "String",
     ];
+    let field_types: Vec<Type> = dynamic_types.iter().map(|t| parse_type(t)).collect();
     let ast = generate_ast("Customer", &field_types);
     let syntax_tree: File = syn::parse2(ast).unwrap();
     let formatted_code = unparse(&syntax_tree);
