6d598c6618c56c30f2ba11a3dbcfc2ef61562530
amascolo <12738821+amascolo@users.noreply.github.com>
2025-03-15 20:52:47 +0100

    create runtime crate

---
+++ runtime/src/date.rs
@@ -0,0 +1,79 @@
+use crate::date;
+use std::fmt;
+use std::ops::AddAssign;
+use time::format_description::well_known::Iso8601;
+use time::Month;
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Date(pub time::Date);
+
+impl Date {
+    pub const fn new(date: time::Date) -> Self {
+        Date(date)
+    }
+}
+
+impl fmt::Display for Date {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.format(&Iso8601::DATE).unwrap())
+    }
+}
+// TODO Debug only needed until we have Record::Display
+impl fmt::Debug for Date {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl std::str::FromStr for Date {
+    type Err = time::error::Parse;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        time::Date::parse(s, &Iso8601::DEFAULT).map(Date)
+    }
+}
+
+// we need this as Month::try_from isn't const
+pub const fn month_from_int(m: u32) -> Month {
+    match m {
+        1 => Month::January,
+        2 => Month::February,
+        3 => Month::March,
+        4 => Month::April,
+        5 => Month::May,
+        6 => Month::June,
+        7 => Month::July,
+        8 => Month::August,
+        9 => Month::September,
+        10 => Month::October,
+        11 => Month::November,
+        12 => Month::December,
+        _ => unreachable!(),
+    }
+}
+
+#[macro_export]
+macro_rules! date {
+    ($yymmdd:literal) => {{
+        const YEAR: i32 = ($yymmdd / 10000) as i32;
+        const MONTH: time::Month = $crate::month_from_int(($yymmdd / 100) % 100);
+        const DAY: u8 = ($yymmdd % 100) as u8;
+
+        match time::Date::from_calendar_date(YEAR, MONTH, DAY) {
+            Ok(date) => $crate::Date::new(date),
+            _ => unreachable!(),
+        }
+    }};
+}
+
+impl Default for Date {
+    fn default() -> Self {
+        date!(00010101) // dummy
+    }
+}
+
+impl AddAssign for Date {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = rhs; // overwrite dummy
+    }
+}
---
+++ runtime/src/hashmap.rs
@@ -0,0 +1,161 @@
+use hashbrown::hash_map::rayon::IntoParIter;
+use rayon::iter::{FromParallelIterator, IntoParallelIterator, ParallelIterator};
+use std::fmt;
+use std::hash::Hash;
+use std::ops::{AddAssign, Deref, DerefMut, Index, IndexMut};
+
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct HashMap<K, V>(hashbrown::HashMap<K, V>)
+where
+    K: Eq + Hash;
+
+impl<K, V> fmt::Display for HashMap<K, V>
+where
+    K: fmt::Display + Ord + Eq + Hash,
+    V: fmt::Display + Ord,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut entries: Vec<_> = self.iter().collect();
+        entries.sort();
+        let entries: Vec<_> = self
+            .iter()
+            .map(|(k, v)| format!("{} -> {}", k, v))
+            .collect();
+        write!(f, "{{\n    {}\n}}", entries.join(",\n    "))
+    }
+}
+
+impl<K, V> HashMap<K, V>
+where
+    K: Eq + Hash,
+{
+    pub fn new() -> Self {
+        HashMap(hashbrown::HashMap::new())
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        HashMap(hashbrown::HashMap::with_capacity(capacity))
+    }
+}
+
+impl<K, V> HashMap<K, V>
+where
+    K: Copy + Eq + Hash,
+    V: AddAssign + Default,
+{
+    pub fn sum(mut self, other: Self) -> Self {
+        self += other;
+        self
+    }
+}
+
+impl<K, V> AddAssign for HashMap<K, V>
+where
+    K: Copy + Eq + Hash,
+    V: AddAssign + Default,
+{
+    fn add_assign(&mut self, other: Self) {
+        for (key, val) in other {
+            self[&key] += val;
+        }
+    }
+}
+
+impl<K, V> Deref for HashMap<K, V>
+where
+    K: Eq + Hash,
+{
+    type Target = hashbrown::HashMap<K, V>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<K, V> DerefMut for HashMap<K, V>
+where
+    K: Eq + Hash,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<K, V> Index<&K> for HashMap<K, V>
+where
+    K: Eq + Hash,
+{
+    type Output = V;
+
+    fn index(&self, key: &K) -> &Self::Output {
+        &self.0[key]
+    }
+}
+
+impl<K, V> IndexMut<&K> for HashMap<K, V>
+where
+    K: Copy + Eq + Hash,
+    V: Default,
+{
+    fn index_mut(&mut self, key: &K) -> &mut Self::Output {
+        self.entry(*key).or_default()
+    }
+}
+
+impl<K, V> IntoIterator for HashMap<K, V>
+where
+    K: Eq + Hash,
+{
+    type Item = (K, V);
+    type IntoIter = hashbrown::hash_map::IntoIter<K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl<K, V> FromIterator<(K, V)> for HashMap<K, V>
+where
+    K: Eq + Hash,
+{
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+        HashMap(iter.into_iter().collect())
+    }
+}
+
+impl<K, V> IntoParallelIterator for HashMap<K, V>
+where
+    K: Eq + Hash + Send,
+    V: Send,
+{
+    type Iter = IntoParIter<K, V>;
+    type Item = (K, V);
+
+    fn into_par_iter(self) -> Self::Iter {
+        self.0.into_par_iter()
+    }
+}
+
+impl<K, V> FromParallelIterator<(K, V)> for HashMap<K, V>
+where
+    K: Eq + Hash + Send,
+    V: Send,
+{
+    fn from_par_iter<T: IntoParallelIterator<Item = (K, V)>>(iter: T) -> Self {
+        HashMap(iter.into_par_iter().collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut map: HashMap<(), u8> = HashMap::new();
+        map.insert((), 0);
+        assert_eq!(map[&()], 0);
+        map[&()] += 1;
+        assert_eq!(map[&()], 1);
+    }
+}
