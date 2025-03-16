7d8f837e2671381007aa0a9ab7d18e6d0f10e745
amascolo <12738821+amascolo@users.noreply.github.com>
2025-03-16 20:36:41 +0100

    fixed e2e tests

--- src/cli.rs
+++ src/cli.rs
@@ -1,37 +1,38 @@
 use crate::rs;
 use std::fs::{self, File};
-use std::io::Write;
+use std::io::{self, Write};
 use std::process::Command;
 
-pub fn run_tpch(query: u8, sf: &str) -> () {
+pub fn run_tpch(query: u8, sf: &str) -> Option<()> {
     let path = format!("{}/progs/tpch/q{query}.sdql", env!("CARGO_MANIFEST_DIR"));
     let src = fs::read_to_string(&path).unwrap().replace(
         "datasets/tpch/",
         &format!("../datasets/tpch_datasets/SF_{sf}/"),
     );
     let code = rs!(&src);
-    run(&code);
+    run(&code)
 }
 
-pub fn run(code: &str) -> () {
+pub fn run(code: &str) -> Option<()> {
     File::create("generated/src/main.rs")
-        .unwrap()
+        .ok()?
         .write_all(code.as_bytes())
-        .unwrap();
+        .ok()?;
 
     let output = Command::new("cargo")
         .arg("run")
         .arg("--release")
         .current_dir("generated")
         .output()
-        .unwrap();
+        .ok()?;
 
     if output.status.success() {
         let stdout = String::from_utf8_lossy(&output.stdout);
-        println!("{stdout}");
+        io::stdout().write_all(stdout.as_bytes()).ok()
     } else {
         let stderr = String::from_utf8_lossy(&output.stderr);
-        let stderr = stderr.replace("--> src/main.rs:", "--> generated/src/main.rs:");
-        eprintln!("{stderr}");
+        let stderr = stderr.replace("src/main.rs:", "generated/src/main.rs:");
+        writeln!(io::stderr(), "{stderr}").ok()?;
+        None
     }
 }
--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,12 +1,11 @@
 use sdql::cli::run_tpch;
 
-// FIXME make it fail when it prints output!
 #[test]
 fn tpch() {
-    run_tpch(1, "0.01");
-    run_tpch(3, "0.01");
-    run_tpch(5, "0.01");
-    run_tpch(6, "0.01");
-    run_tpch(9, "0.01");
-    run_tpch(18, "0.01");
+    run_tpch(1, "0.01").unwrap();
+    run_tpch(3, "0.01").unwrap();
+    run_tpch(5, "0.01").unwrap();
+    run_tpch(6, "0.01").unwrap();
+    run_tpch(9, "0.01").unwrap();
+    run_tpch(18, "0.01").unwrap();
 }
