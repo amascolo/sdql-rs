6ef4437acaa605917012c46a831d9440fc0e95b5
amascolo <12738821+amascolo@users.noreply.github.com>
2025-03-21 23:46:54 +0000

    removed dead code

--- src/tpch/q3/mod.rs
+++ src/tpch/q3/mod.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use sdql_runtime::{date, Bool, Date, HashMap, OrderedFloat, Record};
 
 pub mod parallel;
@@ -7,20 +6,3 @@ pub mod sequential;
 pub type TypeQ3 = HashMap<Record<(i32, Date, i32, OrderedFloat<f64>)>, Bool>;
 
 const _19950315: Date = date!(19950315);
-
-pub fn format_q3_result(result: &TypeQ3) -> String {
-    result
-        .iter()
-        .map(|(key, val)| {
-            format!(
-                "<{},{},{},{}>:{}\n",
-                key.0,
-                key.1,
-                key.2,
-                key.3,
-                (**val) as i32,
-            )
-        })
-        .sorted()
-        .collect()
-}
