6b4ba34fcd283ad88e7f9555f209fd2ddcb553c1
amascolo <12738821+amascolo@users.noreply.github.com>
2025-03-21 20:28:24 +0000

    stupid int newtype

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,6 +1,7 @@
 mod bool;
 mod date;
 mod hashmap;
+mod int;
 mod load;
 mod real;
 mod record;
@@ -10,6 +11,7 @@ pub use bool::{Bool, FALSE, TRUE};
 pub use csv;
 pub use date::{month_from_int, Date};
 pub use hashmap::{HashMap, HashSet};
+pub use int::Int;
 pub use ordered_float::OrderedFloat;
 pub use real::Real;
 pub use record::Record;
