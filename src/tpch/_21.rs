029f79060530f42b9c42ff7c2700dd8b836710ca
amascolo <12738821+amascolo@users.noreply.github.com>
2025-05-01 00:10:14 +0100

    q21 WIP

--- src/tpch/_21.rs
+++ src/tpch/_21.rs
@@ -20,73 +20,68 @@ pub fn tpch_21(
         .filter(|&i| nation_indexed.contains_key(&supplier.3[i as usize]))
         .map(|i| (supplier.0[i as usize], supplier.1[i as usize]))
         .collect();
-    let mut ord_indexed: Vec<i32> = (0..orders.9)
+    // TODO update code generator
+    // let mut ord_indexed: Vec<i32> = (0..orders.9)
+    //     .into_iter()
+    //     .filter(|&i| orders.2[i as usize] == VarChar::from_str("F").unwrap())
+    //     .fold(vec![i32::default(); 6000001], |mut acc: Vec<i32>, i| {
+    //         acc[orders.0[i as usize] as usize] += 1i32;
+    //         acc
+    //     });
+    let mut ord_indexed: Vec<i32> = vec![i32::default(); 6000001];
+    let ptr = ord_indexed.as_mut_ptr() as usize;
+    (0..orders.9)
         .into_iter()
         .filter(|&i| orders.2[i as usize] == VarChar::from_str("F").unwrap())
-        .fold(vec![i32::default(); 6000001], |mut acc: Vec<i32>, i| {
-            acc[orders.0[i as usize] as usize] += 1i32;
-            acc
+        .for_each(move |i| unsafe {
+            *(ptr as *mut i32).add(orders.0[i as usize] as usize) += 1;
         });
-    let mut l2_indexed: Vec<SmallVecDict<[i32; 0usize]>> = (0..lineitem.16).into_iter().fold(
-        vec![SmallVecDict::default(); 6000001],
-        |mut acc: Vec<SmallVecDict<[i32; 0usize]>>, i| {
-            acc[lineitem.0[i as usize] as usize][lineitem.2[i as usize]] += 1i32;
-            acc
-        },
-    );
-    let mut l3_indexed: Vec<SmallVecDict<[i32; 0usize]>> = (0..lineitem.16)
+    // TODO update code generator
+    // let mut l2_indexed: Vec<SmallVecDict<[i32; 0usize]>> = (0..lineitem.16).into_iter().fold(
+    //     vec![SmallVecDict::default(); 6000001],
+    //     |mut acc: Vec<SmallVecDict<[i32; 0usize]>>, i| {
+    //         acc[lineitem.0[i as usize] as usize][lineitem.2[i as usize]] += 1i32;
+    //         acc
+    //     },
+    // );
+    let mut l2_indexed: Vec<SmallVecDict<[i32; 0usize]>> = vec![SmallVecDict::default(); 6000001];
+    let ptr = l2_indexed.as_mut_ptr() as usize;
+    (0..lineitem.16).into_iter().for_each(move |i| unsafe {
+        (&mut (*(ptr as *mut SmallVecDict<[i32; 0usize]>).add(lineitem.0[i as usize] as usize)))
+            [lineitem.2[i as usize]] += 1;
+    });
+    // TODO update code generator
+    // let mut l3_indexed: Vec<SmallVecDict<[i32; 0usize]>> = (0..lineitem.16)
+    //     .into_iter()
+    //     .filter(|&i| lineitem.11[i as usize] < lineitem.12[i as usize])
+    //     .fold(
+    //         vec![SmallVecDict::default(); 6000001],
+    //         |mut acc: Vec<SmallVecDict<[i32; 0usize]>>, i| {
+    //             acc[lineitem.0[i as usize] as usize][lineitem.2[i as usize]] += 1i32;
+    //             acc
+    //         },
+    //     );
+    let mut l3_indexed: Vec<SmallVecDict<[i32; 0usize]>> = vec![SmallVecDict::default(); 6000001];
+    let ptr = l3_indexed.as_mut_ptr() as usize;
+    (0..lineitem.16)
         .into_iter()
-        .map(|i| {
-            (
-                i,
-                Record::new((
-                    lineitem.0[i as usize],
-                    lineitem.1[i as usize],
-                    lineitem.2[i as usize],
-                    lineitem.3[i as usize],
-                    lineitem.4[i as usize],
-                    lineitem.5[i as usize],
-                    lineitem.6[i as usize],
-                    lineitem.7[i as usize],
-                    lineitem.8[i as usize],
-                    lineitem.9[i as usize],
-                    lineitem.10[i as usize],
-                    lineitem.11[i as usize],
-                    lineitem.12[i as usize],
-                    lineitem.13[i as usize],
-                    lineitem.14[i as usize],
-                    lineitem.15[i as usize],
-                )),
-            )
-        })
-        .filter(|&(i, l)| lineitem.11[i as usize] < lineitem.12[i as usize])
-        .fold(
-            vec![SmallVecDict::default(); 6000001],
-            |mut acc: Vec<SmallVecDict<[i32; 0usize]>>, (i, l)| {
-                acc[lineitem.0[i as usize] as usize][lineitem.2[i as usize]] += 1i32;
-                acc
-            },
-        );
+        .filter(|&i| lineitem.11[i as usize] < lineitem.12[i as usize])
+        .for_each(move |i| unsafe {
+            (&mut *(ptr as *mut SmallVecDict<[i32; 0usize]>)
+                .add(lineitem.0[i as usize] as usize))[lineitem.2[i as usize]] += 1;
+        });
     let mut l1_probed: HashMap<Record<(VarChar<25>,)>, Record<(i32,)>> = (0..lineitem.16)
         .into_iter()
-        .map(|i| (i, l2_indexed[lineitem.0[i as usize] as usize].len() as i32))
-        .map(|(i, l2_size)| {
-            (
-                i,
-                l2_size,
-                l3_indexed[lineitem.0[i as usize] as usize].len() as i32,
-            )
-        })
-        .filter(|&(i, l2_size, l3_size)| {
+        .filter(|&i| {
             lineitem.11[i as usize] < lineitem.12[i as usize]
                 && su_probed.contains_key(&lineitem.2[i as usize])
                 && ord_indexed[lineitem.0[i as usize] as usize] != 0
-                && 1i32 < l2_size
-                && l3_size <= 1i32
+                && 1i32 < l2_indexed[lineitem.0[i as usize] as usize].len() as i32
+                && l3_indexed[lineitem.0[i as usize] as usize].len() as i32 <= 1i32
         })
         .fold(
             HashMap::default(),
-            |mut acc: HashMap<Record<(VarChar<25>,)>, Record<(i32,)>>, (i, l2_size, l3_size)| {
+            |mut acc: HashMap<Record<(VarChar<25>,)>, Record<(i32,)>>, i| {
                 acc[&Record::new((su_probed[&lineitem.2[i as usize]],))] += Record::new((1i32,));
                 acc
             },
@@ -104,4 +99,62 @@ pub fn tpch_21_parallel(
     nation: &Nation,
 ) -> TypeQ21 {
     todo!()
+    // let mut nation_indexed: HashMap<i32, Record<(i32,)>> = (0..nation.4)
+    //     .into_par_iter()
+    //     .filter(|&i| nation.1[i as usize] == VarChar::from_str("SAUDI ARABIA").unwrap())
+    //     .map(|i| (nation.0[i as usize], Record::new((nation.0[i as usize],))))
+    //     .collect();
+    // let mut su_probed: HashMap<i32, VarChar<25>> = (0..supplier.7)
+    //     .into_par_iter()
+    //     .filter(|&i| nation_indexed.contains_key(&supplier.3[i as usize]))
+    //     .map(|i| (supplier.0[i as usize], supplier.1[i as usize]))
+    //     .collect();
+    // // TODO update code generator
+    // let mut ord_indexed: Vec<i32> = vec![i32::default(); 6000001];
+    // let ptr = ord_indexed.as_mut_ptr() as usize;
+    // (0..orders.9)
+    //     .into_par_iter()
+    //     .filter(|&i| orders.2[i as usize] == VarChar::from_str("F").unwrap())
+    //     .for_each(|i| unsafe {
+    //         *(ptr as *mut i32).add(orders.0[i as usize] as usize) += 1;
+    //     });
+    // // TODO update code generator
+    // let mut l2_indexed: Vec<SmallVecDict<[i32; 0usize]>> = vec![SmallVecDict::default(); 6000001];
+    // let ptr = l2_indexed.as_mut_ptr() as usize;
+    // (0..lineitem.16).into_par_iter().for_each(|i| unsafe {
+    //     (&mut (*(ptr as *mut SmallVecDict<[i32; 0usize]>).add(lineitem.0[i as usize] as usize)))
+    //         [lineitem.2[i as usize]] += 1;
+    // });
+    // // TODO update code generator
+    // let mut l3_indexed: Vec<SmallVecDict<[i32; 0usize]>> = vec![SmallVecDict::default(); 6000001];
+    // let ptr = l3_indexed.as_mut_ptr() as usize;
+    // (0..lineitem.16)
+    //     .into_par_iter()
+    //     .filter(|&i| lineitem.11[i as usize] < lineitem.12[i as usize])
+    //     .for_each(|i| unsafe {
+    //         (&mut *(ptr as *mut SmallVecDict<[i32; 0usize]>)
+    //             .add(lineitem.0[i as usize] as usize))[lineitem.2[i as usize]] += 1;
+    //     });
+    // // FIXME
+    // let mut l1_probed: HashMap<Record<(VarChar<25>,)>, Record<(i32,)>> = (0..lineitem.16)
+    //     .into_par_iter()
+    //     .filter(|&i| {
+    //         lineitem.11[i as usize] < lineitem.12[i as usize]
+    //             && su_probed.contains_key(&lineitem.2[i as usize])
+    //             && ord_indexed[lineitem.0[i as usize] as usize] != 0
+    //             && 1i32 < l2_indexed[lineitem.0[i as usize] as usize].len() as i32
+    //             && l3_indexed[lineitem.0[i as usize] as usize].len() as i32 <= 1i32
+    //     })
+    //     .fold(
+    //         HashMap::default,
+    //         |mut acc: HashMap<Record<(VarChar<25>,)>, Record<(i32,)>>, i| {
+    //             acc[&Record::new((su_probed[&lineitem.2[i as usize]],))] += Record::new((1i32,));
+    //             acc
+    //         },
+    //     )
+    //     .sum();
+    // l1_probed
+    //     .into_par_iter()
+    //     .map(|(k, v)| (Record::new((k.0, v.0)), TRUE))
+    //     .collect()
 }
