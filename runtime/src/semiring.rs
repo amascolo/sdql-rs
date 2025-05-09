f2adb2fa295c44d2acdb1a26d5c9a6b9656be390
amascolo <12738821+amascolo@users.noreply.github.com>
2025-05-09 19:18:54 +0100

    semirings

--- runtime/src/semiring.rs
+++ runtime/src/semiring.rs
@@ -1,8 +1,28 @@
 use std::marker::ConstParamTy;
 
-#[derive(Clone, Copy, PartialEq, Eq, ConstParamTy)]
+#[derive(PartialEq, Eq, ConstParamTy)]
 pub enum Addition {
     Plus,
     Min,
     Max,
 }
+
+// TODO semiring logic should be modelled on scalars, not hashmap
+//  then we overload AddAssign += and MulAssign *= on the scalars
+//
+// pub struct Promote<T, const SR: SemiRing = { SemiRing::SumProduct }>(T)
+// where
+//     T: AddAssign + MulAssign + Ord;
+//
+// // see table, pg. 5 https://dl.acm.org/doi/10.1145/3527333
+// #[derive(PartialEq, Eq, ConstParamTy)]
+// pub enum SemiRing {
+//     SumProduct,
+//     MinProduct, // mnpr
+//     MaxProduct, // mxpr
+//     MinSum,     // mnsm
+//     MaxSum,     // mxsm
+//     MaxMin,     // mxmn
+// }
+//
+// // TODO macros e.g. mnpr!(x) as Promo::<_, SemiRing::MinProduct>(x)
