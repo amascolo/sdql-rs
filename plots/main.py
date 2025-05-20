7cbfbb5410fb131d07e39df7fc32eee8f37dbafb
amascolo <12738821+amascolo@users.noreply.github.com>
2025-05-20 23:33:28 +0100

    added openpyxl

--- plots/main.py
+++ plots/main.py
@@ -5,11 +5,6 @@ import pandas as pd
 import seaborn as sns
 
 if __name__ == "__main__":
-    major, minor = sys.version_info[:2]
-    assert (major, minor) == (3, 13)
+    df = pd.read_excel("../benchmarks.xlsx")
 
-    assert pd.__version__.startswith("2.2")
-    assert matplotlib.__version__.startswith("3.10")
-    assert sns.__version__.startswith("0.13")
-
-    print("Hello, plotters.")
+    print(df.head())
