f18295cbfcda3c96a9b362fd7f1ab26aab020b40
amascolo <12738821+amascolo@users.noreply.github.com>
2025-09-16 19:48:22 -0700

    generic_arg_infer + let_chains stabilised

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 #![feature(assert_matches)]
 #![feature(box_patterns)]
-#![feature(generic_arg_infer)]
 #![feature(impl_trait_in_bindings)]
-#![feature(let_chains)]
 
 pub mod backend;
 pub mod cli;
